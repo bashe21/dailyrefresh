d20b2139dad50246bec202a49e859e2392369d2c
zyx <3064662835@qq.com>
2019-03-03 18:30:33 +0800

    注册邮箱验证

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,14 +1,57 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
+from django.views.generic import View
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.mail import send_mail
 import re
 from user.models import User
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired
 # Create your views here.
 
 
 # /user/register
 def register(request):
     '''显示注册页面'''
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # 数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户不存在，可用
+            user = None
+
+        if user:
+            # 用户名已存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+        # 进行业务处理 用户注册
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+        # 返回应答, 跳转首页
+        return redirect(reverse('goods:index'))
 
 
 # /user/reigister_handler
@@ -42,9 +85,100 @@ def register_handle(request):
     if user:
         # 用户名已存在
         return render(request, 'register.html', {'errmsg': '用户名已存在'})
-    # 进行业务处理
+    # 进行业务处理 用户注册
     user = User.objects.create_user(username, email, password)
     user.is_active = 0
     user.save()
     # 返回应答, 跳转首页
     return redirect(reverse('goods:index'))
+
+
+class RegisterView(View):
+    '''注册'''
+    def get(self, request):
+        '''显示注册页面'''
+        return render(request, 'register.html')
+
+    def post(self, request):
+        # 接收数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # 数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        # 校验邮箱
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意协议'})
+
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户不存在，可用
+            user = None
+
+        if user:
+            # 用户名已存在
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+        # 进行业务处理 用户注册
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        # 发送激活邮件，包含激活链接：http://127.0.0.1:8000/user/active/1
+        # 激活链接中需要包含用户的身份信息, 并且要把身份信息进行加密
+
+        # 加密用户的身份信息，生成激活的token
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        info = {'confirm': user.id}
+        token = serializer.dumps(info)
+        token = token.decode('utf8')
+
+        # 发邮件
+        subject = '天天生鲜欢迎信息'
+        message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
+        sender = settings.EMAIL_FROM
+        receiver = [email]
+        html_message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
+        send_mail(subject, message, sender, receiver, html_message=html_message)
+
+        # 返回应答, 跳转首页
+        return redirect(reverse('goods:index'))
+
+
+class ActiveView(View):
+    '''用户激活'''
+    def get(self, request, token):
+        '''进行用户激活'''
+        # 进行解密， 获取要激活的用户信息
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        try:
+            info = serializer.loads(token)
+            # 获取激活用户的id
+            user_id = info['confirm']
+
+            # 根据id获取用户信息
+            user = User.objects.get(id=user_id)
+            user.is_active = 1
+            user.save()
+
+            # 跳转登录页面
+            return redirect(reverse('user:login'))
+        except SignatureExpired as e:
+            # 激活链接已经过期
+            return HttpResponse('激活链接已过期')
+
+
+class LoginView(View):
+    '''登录'''
+    def get(self, request):
+        '''显示登录页面'''
+        return render(request, 'login.html')
