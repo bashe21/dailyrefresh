6ecaac2785ccbb0fdc81d614149abded45fccc86
zyx <3064662835@qq.com>
2019-03-03 20:51:38 +0800

    celery发邮件

--- apps/user/views.py
+++ apps/user/views.py
@@ -9,6 +9,8 @@ from user.models import User
 
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
+from celery_tasks.tasks import send_register_active_email
+
 # Create your views here.
 
 
@@ -143,12 +145,15 @@ class RegisterView(View):
         token = token.decode('utf8')
 
         # 发邮件
-        subject = '天天生鲜欢迎信息'
-        message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
-        sender = settings.EMAIL_FROM
-        receiver = [email]
-        html_message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
-        send_mail(subject, message, sender, receiver, html_message=html_message)
+        # subject = '天天生鲜欢迎信息'
+        # message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
+        # sender = settings.EMAIL_FROM
+        # receiver = [email]
+        # html_message = '<h1>%s, 欢迎您成为天天生鲜注册会员</h1>请点击以下链接激活您的账户<br><a href="http://127.0.0.1:8000/user/active/%s">http://127.0.0.1:8000/user/active/%s</a>' % (username, token, token)
+        # send_mail(subject, message, sender, receiver, html_message=html_message)
+
+        send_register_active_email.delay(email, username, token)
+
 
         # 返回应答, 跳转首页
         return redirect(reverse('goods:index'))
